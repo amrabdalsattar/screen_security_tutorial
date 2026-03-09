90151dbf8315f69fa2e4c3c920f2c63e612ef927
amrabdalsattar <amrmmohamed00@gmail.com>
2026-03-09 06:37:12 +0200

    Revert "- FInished YT demo"
    This reverts commit aba3e4dd729ead6cbacf540321ff63c4221f6773.

--- lib/core/extension/extensions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:screen_security_tutorial/core/security/secure_widget.dart';
-
-extension Secure on Widget {
-  Widget secure() => SecureWidget(child: this);
-}
